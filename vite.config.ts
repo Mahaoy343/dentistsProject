0108bfcaf1a0f3073cacd704037e6ed7f7e694e5
maha <mahahiloa6@gmail.com>
2025-08-27 02:44:06 +0300

    Update vite.config.ts with base for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/dentistsProject/', // 👈 اسم المستودع حقك بالضبط
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-});
+})
